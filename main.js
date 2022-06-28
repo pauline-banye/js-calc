d6981a0d8d67c074684db7bfdec6da5dc856498f
Pauline.B <paulinebanye@gmail.com>
2022-06-28 13:52:23 +0100

    calc update

--- main.js
+++ main.js
@@ -6,15 +6,6 @@ function runCalc() {
     // assign variable for the result
     let answer;
 
-    // prompt for the operation to be performed
-    operator = prompt(
-        'Enter the operation you want to perform \n Input + for Addition \n Input - for Subtraction \n Input * for Multiplication or \n Input / for division'
-    );
-    // if the input is not valid, alert the user and exit
-    if (!["+","-","*","/"].includes(operator)) {
-        return alert('Error! This is not a valid operator. Goodbye!');
-    }
-
     // prompt user to input the first number
     let firstNumber = prompt(
         'Enter the first number: '
@@ -24,6 +15,15 @@ function runCalc() {
     let secondNumber = prompt(
         'Enter the second number: '
         );
+
+    // prompt for the operation to be performed
+    operator = prompt(
+        'Enter the operation you want to perform \n Input + for Addition \n Input - for Subtraction \n Input * for Multiplication or \n Input / for division'
+    );
+    // if the input is not valid, alert the user and exit
+    if (!["+","-","*","/"].includes(operator)) {
+        return alert('Error! This is not a valid operator. Goodbye!');
+    }
 
 
     // if the operator is +, add the two numbers
